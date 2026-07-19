21f21d7fb6e11b259e84cf6f0983fc754cc3a3ce
Krish Naharki <krishnaharki827@gmail.com>
2026-07-19 21:43:28 +0545

    fix: remove accidental nested .github directory

--- index.js
+++ index.js
@@ -2,25 +2,16 @@ const express = require('express');
 const lodash = require('lodash');
 const minimatch = require('minimatch');
 
-
 const app = express();
-
 
 app.get('/', (req, res) => {
   const obj = lodash.cloneDeep({
-<<<<<<< HEAD
     message: 'Hello, DevSecOps! I am Krish Naharki, Welcome to the world of secure coding and DevSecOps_practices in cloud class.',
-=======
-    message: 'Hello, DevSecOps by Krish Naharbrhffehhf dnc ndf jndfni!, Welcome to the world of secure coding and DevSecOps practices in cloud class.',
->>>>>>> main
   });
-
 
   const pattern = minimatch('.js', '.js');
 
-
   res.send(`${obj.message} (Pattern match: ${pattern})`);
 });
-
 
 module.exports = app;
